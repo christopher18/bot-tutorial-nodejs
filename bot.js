53e5e63257a0a0bbce3cd29a336700524f306226
Chris Acker <christopheracker1@gmail.com>
2018-06-01 18:56:24 -0400

    Light change towards dadness

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /cool guy$/;
+      botRegex = /I'm+|I am+/i;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = "Yeah it's me";
+  botResponse = "Hi, I'm dadbot!";
 
   options = {
     hostname: 'api.groupme.com',
