620da915bf944a87c28c718259a80201a9065af5
Chris Acker <christopheracker1@gmail.com>
2018-06-01 19:31:15 -0400

    update

--- bot.js
+++ bot.js
@@ -6,10 +6,10 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /(I am)+|(I'm)+/i;
-  let message = request.text
-  if(message && (message.search(/I'm/) != -1 || message.search(/I am/) != -1)) {
+
+  if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
-    postMessage();
+    postMessage(request.text);
     this.res.end();
   } else {
     console.log("don't care");
@@ -18,10 +18,10 @@ function respond() {
   }
 }
 
-function postMessage() {
+function postMessage(message) {
   var botResponse, options, body, botReq;
 
-  botResponse = "Hi there!";
+  botResponse = message;
 
   options = {
     hostname: 'api.groupme.com',
