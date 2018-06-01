21866eb3b7ed15b64a27e1dfb72e54d3c3298742
Chris Acker <christopheracker1@gmail.com>
2018-06-01 00:46:01 -0400

    testing original

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = "Yeah it's me";
 
   options = {
     hostname: 'api.groupme.com',
