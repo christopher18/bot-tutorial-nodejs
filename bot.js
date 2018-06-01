81020ae81e799edc376fa30a4e383f22f49873a8
Chris Acker <christopheracker1@gmail.com>
2018-06-01 19:33:04 -0400

    update

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage(message) {
   var botResponse, options, body, botReq;
 
-  botResponse = message;
+  botResponse = "hello";
 
   options = {
     hostname: 'api.groupme.com',
